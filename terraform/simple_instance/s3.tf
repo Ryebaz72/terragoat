3991385e2ffda0cea59159ee5cef5f77d3f2ae92
Ryebaz72 <Ryebaz72@users.noreply.github.com>
2022-02-10 15:55:49 +0000

    Update s3.tf

--- terraform/simple_instance/s3.tf
+++ terraform/simple_instance/s3.tf
@@ -18,3 +18,40 @@ resource "aws_s3_bucket" "docking_bay" {
     yor_trace            = "ea3e6629-46c2-44d5-a468-706d9fda1e4a"
   }
 }
+
+resource "aws_security_group" "ssh_traffic" {
+  name        = "ssh_traffic"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server_instance" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  security_groups = [ "${aws_security_group.ssh_traffic.name}" ]
+  tags = {
+    Name = "bc_workshop_ec2"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
